d49ab2d83c8c21138d040f85a365e4cdfa792b25
hariharan-km <hariharank6676@gmail.com>
2024-12-07 18:41:06 +0530

    Up:New Projects

---
+++ ToDo-App/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDo from './Components/ToDo'
+
+const App = () => {
+  return (
+    <div className='bg-stone-900	grid py-4	h-screen'>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
